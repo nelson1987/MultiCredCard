4946553ede5b51b292058ffeb4b4cd7375262174
Nelson Neto <nelson.ash@outlook.com>
2017-10-06 11:55:32 -0300

    Primeiro Commit
    Commit com teste para validar se o deploy está funcional

---
+++ MultiCredCard/MultiCredCard.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MultiCredCard.Domain.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TesteValidador()
+        {
+            var um = 1;
+            Assert.AreEqual(um, 1);
+        }
+    }
+}
